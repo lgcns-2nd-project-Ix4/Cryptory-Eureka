df4a90d85ffb6b440ae42f250f8897ead955ace8
gwangbu-desu <kwy1379@ajou.ac.kr>
2025-03-29 18:42:37 +0900

    Fix: 파일명 수정

--- src/test/java/com/cryptory/eureka/EurekaApplicationTests.java
+++ src/test/java/com/cryptory/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cryptory.gateway;
+package com.cryptory.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
